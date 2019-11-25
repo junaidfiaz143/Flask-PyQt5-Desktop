d6f4cca31e4419c88a8b4bff167ad795f49f56a6
jd <junaidfiaz143@gmail.com>
2019-11-25 16:37:46 +0500

    update -add- hide console function

---
+++ app.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt5.QtGui import QCursor
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+class MyApp(QWidget):
+
+	def __init__(self):
+		super().__init__()
+		QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
+		self.initUI()
+
+	def closeEvent(self, event):
+
+		msg_box = QMessageBox()
+		msg_box.setIcon(QMessageBox.Warning)
+		msg_box.setWindowTitle("Alert")
+		msg_box.setText("Are you sure you want to exit the program?")
+		msg_box.setWindowIcon(QIcon("resources/icons/trello.ico"))
+		msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+
+		retval = msg_box.exec_()
+
+		if retval == QMessageBox.Yes:
+			event.accept()
+		else:
+			event.ignore()
+
+	def initUI(self):
+
+		self.web = QWebEngineView()
+		self.web.setWindowTitle("Trello")
+		self.web.setWindowIcon(QIcon("resources/icons/trello.ico"))
+		self.web.load(QUrl("https://trello.com/"))
+		self.web.showMaximized()
+		self.web.setContextMenuPolicy(Qt.NoContextMenu)
+		self.web.show()
+		self.web.loadFinished.connect(self.webpageLoaded)
+
+		self.web.closeEvent = self.closeEvent
+
+	def webpageLoaded(self):
+		QApplication.restoreOverrideCursor()
+
+def hide_console():
+		import win32gui, win32con
+
+		cmd_terminal = win32gui.GetForegroundWindow()
+		win32gui.ShowWindow(cmd_terminal , win32con.SW_HIDE)
+
+if __name__ == '__main__':
+	hide_console()
+
+	app = QApplication(sys.argv)
+	my_app = MyApp()
+	sys.exit(app.exec_())
